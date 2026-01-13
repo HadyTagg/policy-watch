a8f76ae7797afdbc096acbed94970ef8027382ab
Hady Tagg <hadytagg@mail.com>
2026-01-13 18:21:35 +0000

    fixed bold text in staff details table.

--- src/policywatch/ui/main.py
+++ src/policywatch/ui/main.py
@@ -1760,7 +1760,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.audit_table.setFont(audit_font)
 
         self.audit_table.setStyleSheet(
-            "QTableWidget::item { color: white; }"
+            "QTableWidget::item { color: white; font-weight: bold;}"
             "QTableWidget::item:selected { background-color: blue; color: white; }"
         )
 
